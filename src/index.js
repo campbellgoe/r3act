dba24faf75f03093b3a56894273c6f6c43728924
George Campbell <campbell.goe@gmail.com>
2019-05-27 00:20:03 +0100

    check WebGL available, if not display message

--- src/index.js
+++ src/index.js
@@ -3,8 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import WebGLAvailability from './WebGLAvailability';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+if (WebGLAvailability.isWebGLAvailable()) {
+  ReactDOM.render(<App />, document.getElementById('root'));
+} else {
+  var warning = WebGLAvailability.getWebGLErrorMessage();
+  document.documentElement.appendChild(warning);
+}
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
