11a320f07b815760c36e777d70d95a4546194e0a
George Campbell <campbell.goe@gmail.com>
2019-05-27 01:12:01 +0100

    line + cuboid

--- src/Scene.js
+++ src/Scene.js
@@ -10,20 +10,40 @@ class Scene extends Component {
     const scene = new THREE.Scene();
     const camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 1000);
     const renderer = new THREE.WebGLRenderer({ antialias: true });
-    const geometry = new THREE.BoxGeometry(1, 1, 1);
-    const material = new THREE.MeshBasicMaterial({ color: '#433F81' });
-    const cube = new THREE.Mesh(geometry, material);
 
+    //cube
+    const cube = {
+      geometry: new THREE.BoxGeometry(5, 100, 100),
+      material: new THREE.MeshBasicMaterial({ color: '#433F81' }),
+    };
+    cube.entity = new THREE.Mesh(cube.geometry, cube.material);
+
+    //line
+    const line = {
+      geometry: new THREE.Geometry(),
+      material: new THREE.LineBasicMaterial({ color: '#0000ff' }),
+    };
+    const lineVerticies = [
+      new THREE.Vector3(-100, 0, 0),
+      new THREE.Vector3(0, 50, 0),
+      new THREE.Vector3(100, 0, 0),
+    ];
+    line.geometry.vertices.push(...lineVerticies);
+    line.entity = new THREE.Line(line.geometry, line.material);
+    line.mesh = new THREE.Mesh(line.geometry, line.material);
     camera.position.z = 4;
-    scene.add(cube);
+
+    scene.add(cube.entity);
+    scene.add(line.entity);
+
     renderer.setClearColor('#000000');
     renderer.setSize(width, height);
-
+    camera.position.set(0, 0, 100);
+    camera.lookAt(0, 0, 0);
     this.scene = scene;
     this.camera = camera;
     this.renderer = renderer;
-    this.material = material;
-    this.cube = cube;
+    this.cube = cube.entity;
 
     this.mount.appendChild(this.renderer.domElement);
     this.start();
@@ -45,8 +65,9 @@ class Scene extends Component {
   };
 
   animate = () => {
-    this.cube.rotation.x += 0.01;
-    this.cube.rotation.y += 0.01;
+    const cube = this.cube;
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
 
     this.renderScene();
     this.frameId = window.requestAnimationFrame(this.animate);
