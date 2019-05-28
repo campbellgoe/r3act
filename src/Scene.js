804eed67bcb36d87529c6861b82c6e26182fddfc
George Campbell <campbell.goe@gmail.com>
2019-05-28 23:22:18 +0100

    shadows!

--- src/Scene.js
+++ src/Scene.js
@@ -36,15 +36,6 @@ class Scene extends Component {
           loader.load(
             treeGLTF,
             function(gltf) {
-              gltf.scene.traverse(child => {
-                if (child.isMesh && child.name.includes('leaf')) {
-                  //this allows transparent textures such a tree leaves
-                  child.material.transparent = true;
-                  //this removes depth issues where leaves behind were blacked
-                  //out behind leaves in front
-                  child.material.alphaTest = 0.5;
-                }
-              });
               resolve(gltf.scene);
             },
             undefined,
@@ -61,7 +52,7 @@ class Scene extends Component {
       }
     });
   };
-
+  scl = 3;
   componentDidMount() {
     window.THREE = THREE;
 
@@ -75,44 +66,56 @@ class Scene extends Component {
     renderer.gammaOutput = true;
     renderer.gammaFactor = 2.2;
     renderer.shadowMap.enabled = true;
+    renderer.shadowMap.type = THREE.PCFSoftShadowMap;
     //cube
-    const cube = {
-      geometry: new THREE.BoxBufferGeometry(400, 300, 300),
-      material: new THREE.MeshLambertMaterial({ color: '#433F81' }),
-    };
-    cube.entity = new THREE.Mesh(cube.geometry, cube.material);
-    cube.entity.castShadow = true;
-    cube.entity.receiveShadow = true;
-    cube.entity.position.y = 400;
+    // const cube = {
+    //   geometry: new THREE.BoxBufferGeometry(2, 15, 0.5),
+    //   material: new THREE.MeshLambertMaterial({ color: '#433F81' }),
+    // };
+    // cube.entity = new THREE.Mesh(cube.geometry, cube.material);
+    // cube.entity.castShadow = true;
+    // cube.entity.receiveShadow = true;
+    // cube.entity.position.y = 5;
+    // cube.entity.position.x = 8;
+    // cube.entity.position.z = 6;
     //line
-    const line = {
-      geometry: new THREE.Geometry(),
-      material: new THREE.LineBasicMaterial({ color: '#0000ff' }),
-    };
-    const lineVerticies = [
-      new THREE.Vector3(-100, 0, 0),
-      new THREE.Vector3(0, 50, 0),
-      new THREE.Vector3(100, 0, 0),
-    ];
-    line.geometry.vertices.push(...lineVerticies);
-    line.entity = new THREE.Line(line.geometry, line.material);
+    // const line = {
+    //   geometry: new THREE.Geometry(),
+    //   material: new THREE.LineBasicMaterial({ color: '#0000ff' }),
+    // };
+    // const lineVerticies = [
+    //   new THREE.Vector3(-10, 0, 0),
+    //   new THREE.Vector3(0, 5, 0),
+    //   new THREE.Vector3(10, 0, 0),
+    // ];
+    // line.geometry.vertices.push(...lineVerticies);
+    // line.entity = new THREE.Line(line.geometry, line.material);
 
     // White directional light at half intensity shining from the top.
-    var dLight = new THREE.DirectionalLight('#fafaff', 3);
-    dLight.position.set(500, 500, 100);
+    var dLight = new THREE.DirectionalLight('#fafaff', 1);
+    dLight.position.set(400 * this.scl, 1000 * this.scl, 600 * this.scl);
     dLight.castShadow = true;
-    dLight.shadowCameraVisible = true;
+    dLight.shadow.camera.zoom = 1;
+    dLight.shadow.camera.right = 15 * this.scl;
+    dLight.shadow.camera.left = -15 * this.scl;
+    dLight.shadow.camera.top = 20 * this.scl;
+    dLight.shadow.camera.bottom = -10 * this.scl;
+    dLight.shadow.camera.far = 2200 * this.scl;
+    dLight.shadow.mapSize.width = 1024;
+    dLight.shadow.mapSize.height = 1024;
+    //var helper = new THREE.CameraHelper(dLight.shadow.camera);
+    //scene.add(helper);
     scene.add(dLight);
 
-    var aLight = new THREE.AmbientLight(0x404040, 1); // soft white light
+    var aLight = new THREE.AmbientLight(0x404040, 0.4); // soft white light
     scene.add(aLight);
 
     camera.position.z = 4;
 
     const plane = {
-      geometry: new THREE.PlaneGeometry(10000, 10000, 1, 1),
+      geometry: new THREE.PlaneGeometry(3000 * this.scl, 3000 * this.scl, 1, 1),
       material: new THREE.MeshLambertMaterial({
-        color: 0xffff00,
+        color: '#364823',
         side: THREE.FrontSide,
       }),
     };
@@ -121,24 +124,46 @@ class Scene extends Component {
     console.log('plane', plane.entity);
     plane.entity.receiveShadow = true;
     scene.add(plane.entity);
-    scene.add(cube.entity);
-    scene.add(line.entity);
+    //scene.add(cube.entity);
+    //scene.add(line.entity);
 
-    renderer.setClearColor('#000000');
+    renderer.setClearColor('#78a8bb');
     renderer.setSize(width, height);
     camera.position.set(0, 0, 100);
     camera.lookAt(0, 0, 0);
     this.scene = scene;
     this.camera = camera;
     this.renderer = renderer;
-    this.cube = cube.entity;
+    //this.cube = cube.entity;
+    this.dLight = dLight;
 
     this.loadModels()
       .then(obj => {
         console.log('model:', obj);
+        obj.scale.set(0.015 * this.scl, 0.015 * this.scl, 0.015 * this.scl);
+        obj.castShadow = true;
+
         obj.traverse(o => {
           if (o.isMesh) {
             o.castShadow = true;
+
+            if (o.name.includes('leaf')) {
+              //this allows transparent textures such a tree leaves
+              o.material.transparent = true;
+              //this removes depth issues where leaves behind were blacked
+              //out behind leaves in front
+              o.material.alphaTest = 0.5;
+              console.log('material', o.material.map);
+              var customDepthMaterial = new THREE.MeshDepthMaterial({
+                depthPacking: THREE.RGBADepthPacking,
+
+                map: o.material.map, // or, alphaMap: myAlphaMap
+
+                alphaTest: 0.5,
+              });
+
+              o.customDepthMaterial = customDepthMaterial;
+            }
           }
         });
         this.scene.add(obj);
@@ -171,16 +196,18 @@ class Scene extends Component {
 
   animate = ms => {
     ms = Math.round(ms / 10);
-    const cube = this.cube;
+    //const cube = this.cube;
     const cam = this.camera;
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
-    cam.position.set(
-      Math.sin(ms / 160) * 300,
-      Math.cos(ms / 220) * 500 + 500,
-      Math.cos(ms / 300) * 800
-    );
-    var point = new THREE.Vector3(0, Math.sin(ms / 220) * 200 + 200, 0);
+    // cube.rotation.x += 0.01;
+    // cube.rotation.y += 0.01;
+    let x = Math.sin(ms / 160) * 12 * this.scl;
+    let y = Math.cos(ms / 320) * 9 * this.scl + 11 * this.scl;
+    let z = Math.cos(ms / 210) * 12 * this.scl;
+    cam.position.set(x, y, z);
+    //this.dLight.position.set(-x * 10, y * 10, -z * 10);
+    //this.dLight.shadow.camera.updateProjectionMatrix();
+    y = Math.sin(ms / 220) * 5 * this.scl + 5 * this.scl;
+    var point = new THREE.Vector3(0, y, 0);
 
     cam.lookAt(point);
     this.renderScene();
