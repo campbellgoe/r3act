9bad4c44a4ef27e354d740376709e2754a61748d
George Campbell <campbell.goe@gmail.com>
2019-05-30 01:56:26 +0100

    orbital camera, efficient nice shadows

--- src/Scene.js
+++ src/Scene.js
@@ -53,6 +53,15 @@ class Scene extends Component {
     });
   };
   scl = 3;
+  //alpha and beta must be normalized between 0 and Math.PI*2
+  angularDistance = (alpha, beta) => {
+    const pi = Math.PI;
+    const tau = pi * 2;
+    const phi = Math.abs(beta - alpha) % tau; // This is either the distance or 360 - distance
+    const distance = phi > pi ? tau - phi : phi;
+    return distance;
+  };
+  mouseIsDown = false;
   componentDidMount() {
     window.THREE = THREE;
 
@@ -63,7 +72,7 @@ class Scene extends Component {
     const scene = new THREE.Scene();
     const fog = {
       color: skyColour,
-      density: 0.009,
+      density: 0.0009,
     };
     scene.fog = new THREE.FogExp2(fog.color, fog.density);
     const camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 5000);
@@ -106,10 +115,20 @@ class Scene extends Component {
     dLight.shadow.camera.top = 20 * this.scl;
     dLight.shadow.camera.bottom = -10 * this.scl;
     dLight.shadow.camera.far = 2200 * this.scl;
-    dLight.shadow.mapSize.width = 1024;
-    dLight.shadow.mapSize.height = 1024;
+    const maxTS = renderer.capabilities.maxTextureSize;
+    if (maxTS >= 4096) {
+      dLight.shadow.mapSize.width = 4096;
+      dLight.shadow.mapSize.height = 4096;
+    } else if (maxTS >= 2048) {
+      dLight.shadow.mapSize.width = 2048;
+      dLight.shadow.mapSize.height = 2048;
+    } else if (maxTS >= 1024) {
+      dLight.shadow.mapSize.width = 1024;
+      dLight.shadow.mapSize.height = 1024;
+    }
 
     var helper = new THREE.CameraHelper(dLight.shadow.camera);
+    this.helper = helper;
     scene.add(helper);
     scene.add(dLight);
     scene.add(dLight.target);
@@ -140,68 +159,154 @@ class Scene extends Component {
       const w = window.innerWidth;
       const h = window.innerHeight;
       let mx, my;
-      if (e.type === 'mousemove') {
+      if (e.type === 'mousedown') {
+        this.mouseIsDown = true;
+      } else if (e.type === 'mouseup') {
+        this.mouseIsDown = false;
+      }
+      if (e.type.startsWith('mouse')) {
         mx = e.pageX;
         my = e.pageY;
       } else if (e.type === 'touchmove') {
-        mx = w - e.touches[0].pageX;
-        my = h - e.touches[0].pageY;
+        if (e.touches.length === 1) {
+          mx = e.touches[0].pageX;
+          my = h - e.touches[0].pageY;
+          this.mouseIsDown = false;
+        } else if (e.touches.length === 2) {
+          mx = (e.touches[0].pageX + e.touches[1].pageX) / 2;
+          my = (e.touches[0].pageY + e.touches[1].pageY) / 2;
+          this.mouseIsDown = true;
+        }
       }
+      const orbitCamera = () => {
+        const rx = mx / w;
+        const ax = rx * 80 * this.scl - 40 * this.scl;
+        const ry = my / h;
+        const ay = ry * 80 * this.scl - 20 * this.scl;
+        const zoom = Math.max(0.05 * this.scl, ry);
+        const zoomAbs = 40 * this.scl * zoom;
+        const camOrbitAngle = rx * Math.PI * 2;
+        //camera.position.x = ax;
+        camera.position.x = Math.cos(camOrbitAngle) * zoomAbs;
 
-      const rx = mx / w;
-      const ax = rx * 80 - 40;
-      camera.position.x = ax;
+        camera.position.z = Math.sin(camOrbitAngle) * zoomAbs;
+        //camera.position.z = ay;
+        camera.position.y = Math.max(ay, 3 * this.scl);
 
-      const ry = my / h;
-      const ay = ry * 80 - 20;
-      camera.position.z = ay;
-      camera.position.y = ay;
+        //const sl = (w - mx) ** 0.5;
+        //const sr = mx ** 0.5;
+        //use camOrbitAngle to determine shadow camera left, right, top, bottom etc.
+        /*
+        if angle is between 0 and Math.PI/2
+      */
+        const sm = Math.min(Math.max(zoom + 0.5, 0.7), 1.2);
+        let st = 0.1;
+        let sr = 0.1;
+        let sb = 0.1;
+        let sl = 0.1;
+        console.log('aangle:', camOrbitAngle);
+        //camOrbitAngle = camOrbitAngle + Math.PI;
+        sb = this.angularDistance(camOrbitAngle, Math.PI / 4) / Math.PI;
+        sl = this.angularDistance(camOrbitAngle, (Math.PI / 4) * 3) / Math.PI;
+        st =
+          this.angularDistance(camOrbitAngle, Math.PI / 4 + Math.PI) / Math.PI;
+        sr =
+          this.angularDistance(camOrbitAngle, (Math.PI / 4) * 3 + Math.PI) /
+          Math.PI;
+        console.log('sb', sb, 'sl', sl);
+        if (camOrbitAngle < Math.PI / 2) {
+          console.log('btm');
+          //
+        } else if (camOrbitAngle <= Math.PI) {
+          console.log('lft');
+          //((Math.PI/4)*3)
+        } else if (camOrbitAngle <= Math.PI + Math.PI / 2) {
+          console.log('top');
+          //(Math.PI/4)+Math.PI
+        } else {
+          console.log('rgt');
+          //((Math.PI/4)*3)+Math.PI
+        }
+        const sarr = { st, sr, sb, sl };
+        st = sarr.st * 2 + (sarr.sl + sarr.sr);
+        sr = (sarr.sr * 2 + (sarr.st + sarr.sb)) * 1.5;
+        sb = sarr.sb * 2 + (sarr.sr + sarr.sl);
+        sl = (sarr.sl * 2 + (sarr.sb + sarr.st)) * 1.5;
 
-      var raycaster = new THREE.Raycaster();
-      var center = new THREE.Vector2();
+        st = Math.max(0.4, st * 1.3);
+        sr = Math.max(0.4, sr * 1.3);
+        sb = Math.max(0.4, sb * 1.3);
+        sl = Math.max(0.4, sl * 1.3);
+        // st = camOrbitAngle;
+        // sr = st;
+        // sb = st;
+        // sl = st;
+        const sMax = 50 * this.scl;
+        const sMin = 2 * this.scl;
+        dLight.shadow.camera.left =
+          -Math.max(sMin, Math.min(sMax, zoomAbs ** sm * sl)) * this.scl;
+        dLight.shadow.camera.right =
+          Math.max(sMin, Math.min(sMax, zoomAbs ** sm * sr)) * this.scl;
 
-      center.x = 0; //rx * 2 - 1;
-      center.y = 0; //ry * 2 - 1;
+        dLight.shadow.camera.top =
+          Math.max(sMin, Math.min(sMax, zoomAbs ** sm * st)) * this.scl;
+        dLight.shadow.camera.bottom =
+          -Math.max(sMin, Math.min(sMax, zoomAbs ** sm * sb)) * this.scl;
+      };
 
-      raycaster.setFromCamera(center, camera);
+      const translateCamera = () => {
+        var raycaster = new THREE.Raycaster();
+        var center = new THREE.Vector2();
 
-      // calculate objects intersecting the picking ray
-      var intersects = raycaster.intersectObject(plane.entity);
-      let groundPoint;
-      for (var i = 0; i < intersects.length; i++) {
-        groundPoint = intersects[i].point;
-        //intersects[i].object.material.color.set(0xff0000);
-      }
-      if (intersects.length > 1) {
-        throw new Error('how can intersects be > 1?');
-      }
-      if (groundPoint) {
-        //cam view intersects with ground plane at groundPoint
-        //add groundPoint x,y,z to the directional light position and target point.
-        dLight.target.position.copy(groundPoint);
-        dLight.position.set(
-          400 * this.scl + groundPoint.x,
-          1000 * this.scl + groundPoint.y,
-          600 * this.scl + groundPoint.z
-        );
-      }
+        center.x = 0; //rx * 2 - 1;
+        center.y = 0; //ry * 2 - 1;
+        camera.lookAt(0, 0.1 * this.scl, 0);
+        raycaster.setFromCamera(center, camera);
 
-      const sl = (w - mx) ** 0.5;
-      const sr = mx ** 0.5;
-      dLight.shadow.camera.left = -sl - 20;
-      dLight.shadow.camera.right = sr + 20;
+        // calculate objects intersecting the picking ray
+        var intersects = raycaster.intersectObject(plane.entity);
+        let groundPoint;
+        let intersection;
+        if (intersects.length > 1) {
+          console.warn('how can intersects a flat plane be > 1?', intersects);
+          //throw new Error('how can intersects be > 1?');
+        }
+        if (Array.isArray(intersects) && intersects.length > 0) {
+          intersection = intersects[0];
+        }
+        if (intersection) {
+          groundPoint = intersection.point;
+        }
 
-      dLight.shadow.camera.top = 10 * this.scl;
-      dLight.shadow.camera.bottom = -5 * this.scl;
+        if (groundPoint) {
+          //cam view intersects with ground plane at groundPoint
+          //add groundPoint x,y,z to the directional light position and target point.
+          dLight.target.position.copy(groundPoint);
+          dLight.position.set(
+            1000 * this.scl + groundPoint.x,
+            1000 * this.scl + groundPoint.y,
+            1000 * this.scl + groundPoint.z
+          );
+        }
+      };
       // dLight.position.set(
       //   400 * this.scl,
       //   1000 * this.scl,
       //   600 * this.scl
       // );
       // this.dLight.position.set(-x * 10, y * 10, -z * 10);
+      if (this.mouseIsDown) {
+        translateCamera();
+      } else {
+        orbitCamera();
+        translateCamera();
+      }
       dLight.shadow.camera.updateProjectionMatrix();
+      helper.update();
     };
+    document.addEventListener('mousedown', onMove);
     document.addEventListener('mousemove', onMove);
+    document.addEventListener('mouseup', onMove);
     document.addEventListener('touchmove', onMove);
     this.scene = scene;
     this.camera = camera;
@@ -210,36 +315,49 @@ class Scene extends Component {
     this.dLight = dLight;
 
     this.loadModels()
-      .then(obj => {
-        console.log('model:', obj);
-        obj.scale.set(0.015 * this.scl, 0.015 * this.scl, 0.015 * this.scl);
-        obj.castShadow = true;
+      .then(objOriginal => {
+        const objs = [objOriginal];
+        for (let i = 0; i < 100; i++) {
+          const newObj = objOriginal.clone();
+          newObj.position.set(
+            Math.random() * 800 - 400,
+            0,
+            Math.random() * 800 - 400
+          );
+          newObj.rotation.y = Math.random() * Math.PI * 2;
+          objs.push(newObj);
+        }
+        objs.forEach(obj => {
+          console.log('model:', obj);
+          obj.scale.set(0.015 * this.scl, 0.015 * this.scl, 0.015 * this.scl);
+          obj.castShadow = true;
 
-        obj.traverse(o => {
-          if (o.isMesh) {
-            o.castShadow = true;
+          obj.traverse(o => {
+            if (o.isMesh) {
+              o.castShadow = true;
 
-            if (o.name.includes('leaf')) {
-              //this allows transparent textures such a tree leaves
-              o.material.transparent = true;
-              //this removes depth issues where leaves behind were blacked
-              //out behind leaves in front
-              o.material.alphaTest = 0.5;
-              console.log('material', o.material.map);
-              var customDepthMaterial = new THREE.MeshDepthMaterial({
-                depthPacking: THREE.RGBADepthPacking,
+              if (o.name.includes('leaf')) {
+                //this allows transparent textures such a tree leaves
+                o.material.transparent = true;
+                //this removes depth issues where leaves behind were blacked
+                //out behind leaves in front
+                o.material.alphaTest = 0.5;
+                console.log('material', o.material.map);
+                var customDepthMaterial = new THREE.MeshDepthMaterial({
+                  depthPacking: THREE.RGBADepthPacking,
 
-                map: o.material.map, // or, alphaMap: myAlphaMap
+                  map: o.material.map, // or, alphaMap: myAlphaMap
 
-                alphaTest: 0.5,
-              });
+                  alphaTest: 0.5,
+                });
 
-              o.customDepthMaterial = customDepthMaterial;
+                o.customDepthMaterial = customDepthMaterial;
+              }
             }
-          }
+          });
+          this.scene.add(obj);
         });
-        this.trees = obj;
-        this.scene.add(obj);
+        this.trees = objs;
         //       ((obj as THREE.Mesh).material as THREE.Material).transparent = true;
         //((obj as THREE.Mesh).material as THREE.Material).side = THREE.DoubleSide;
         //((obj as THREE.Mesh).material as THREE.Material).alphaTest = 0.5;
@@ -283,6 +401,7 @@ class Scene extends Component {
       var point = new THREE.Vector3(0, y, 0);
 
       cam.lookAt(point);
+      this.helper.update();
     }
     //if (this.trees) this.trees.position.x = Math.sin(ms / 300) * 40;
     //this.dLight.position.set(-x * 10, y * 10, -z * 10);
